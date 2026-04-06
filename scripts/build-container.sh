4c1e0a0fd4fc1ae39076f776b532ece22d5bbe55
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2026-04-06 00:55:14 +0000

    chore: more tags

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -9,5 +9,8 @@ DATE="$(date +'%Y-%m-%d-%H-%M')"
 # containerize that shit
 docker build -t esacteksab/dev-container:"${DATE}" .
 docker tag esacteksab/dev-container:"${DATE}" esacteksab/dev-container:"${TAG}"
+docker tag esacteksab/dev-container:"${DATE}" esacteksab/dev-container:latest
 
-# docker push esacteksab/dev-container:"${DATE}"
+docker push esacteksab/dev-container:"${DATE}"
+docker push esacteksab/dev-container:"${TAG}"
+docker push esacteksab/dev-container:latest
